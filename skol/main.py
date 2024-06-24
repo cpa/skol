a94a64b93f791d458ef1e8be9eb9f11003ffe618
Charles-Pierre Astolfi <8159895-charlespierre@users.noreply.gitlab.com>
2024-06-24 11:25:04 +0200

    version

--- skol/main.py
+++ skol/main.py
@@ -3,13 +3,13 @@ import sys
 
 import click
 
-from formats import FORMATS, JSONFormat
-from guesses import (
+from .formats import FORMATS, JSONFormat
+from .guesses import (
     guess_if_data_is_quoted,
     guess_separator_unquoted,
     split_by_quote_char,
 )
-from llm import ask
+from .llm import ask
 
 
 @click.command()
